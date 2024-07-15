3ccecb563018f03ca5a60777b143f94e52bd212d
Elena176 <lena.tdn176@gmail.com>
2024-07-15 14:45:17 +0300

    changed Elena to Olena

--- src/main/Main.js
+++ src/main/Main.js
@@ -11,9 +11,9 @@ export const Main = () => {
                 <div className={s.text}>
                     <span>Hi there !</span>
                     <h1>
-                        I'm Elena Tokarieva
+                        I'm Olena Tokarieva
                     </h1>
-                    <p>I'm a Front-End Developer based in Odessa, Ukraine. I strives to build immersive and beautiful
+                    <p>I'm a Front-End Developer based in Odesa, Ukraine. I strives to build immersive and beautiful
                         web
                         applications through carefully crafted code and user-centric design.</p>
                 </div>
